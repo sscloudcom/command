6ee28903ed25beda70d4d4a97fce528d01e4344d
sscloudcom <78679707+sscloudcom@users.noreply.github.com>
2021-02-07 16:07:29 +0700

    Create server.ex

---
+++ server.ex
@@ -0,0 +1,14 @@
+server(port)
+server(domain)
+server(host)
+server(dir)
+server(path)
+
+port(8080)
+domain(https://sscloudcom.com)
+host(localhost:8080)
+dir(https://sscloudcom.com/index.ex)
+path(https://)
+
+index.ex(dir/sscloudcom/command/index.ex)
+index.ex(https://sscloudcom.com/)
